19d09e6774e6fb89793dd64a53df95e00fec6d55
FingerLiu <524570375@qq.com>
2017-12-05 18:37:14 +0900

    delelte secrete info

--- app/config.py
+++ app/config.py
@@ -11,8 +11,6 @@ CELERY_BROKER_URL = REDIS_URL
 CELERY_RESULT_BACKEND = REDIS_URL
 CELERYD_MAX_TASKS_PER_CHILD = 1
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:nst3618mysql@35.187.220.29/link_guard'
-# SQLALCHEMY_DATABASE_URI = 'mysql://root:nst3618mysql@35.187.220.29/link_guard'
-CDN = 'https://sstatic.qrpay.paduola.com/'
+
 
 DEPTH_LIMIT = 3
