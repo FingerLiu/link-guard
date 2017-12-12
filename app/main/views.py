509d65f8cdc63bc2f01c3a3dec85c8766baaa2c5
liupeng <524570375@qq.com>
2017-12-12 02:25:08 +0000

    [ADD] add intro

--- app/main/views.py
+++ app/main/views.py
@@ -18,10 +18,11 @@ from ..models import User, Link
 
 @main.route('/')
 def index():
-    # all count
-    all_links = Link.query.filter_by(owner_id=current_user.id).count()
-    # failed count
-    failed_links = Link.query.filter_by(owner_id=current_user.id, status='failed').count()
+    if current_user.is_authenticated:
+        # all count
+        all_links = Link.query.filter_by(owner_id=current_user.id).count()
+        # failed count
+        failed_links = Link.query.filter_by(owner_id=current_user.id, status='failed').count()
     return render_template('index.html')
 
 
